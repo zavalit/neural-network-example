e0a2512a156c89ff0e515e302a01166d05e70e98
Vsevolod Dolgopolov <zavalit@gmail.com>
2019-01-21 16:11:20 +0100

    add method comments

--- src/main/scala/example/NeuralNetwork.scala
+++ src/main/scala/example/NeuralNetwork.scala
@@ -7,6 +7,14 @@ object NeuralNetwork{
 
     val derivate: Tensor => Tensor = (activation: Tensor) => activation * (1 - activation)
 
+
+
+  /**
+    *
+    * @param weights
+    * @param activation
+    * @return weights
+    */
     def activate(weights: List[Tensor], activation: Tensor): List[Tensor] = weights match {
       case Nil => List(activation)
       case weight :: weights_tail => {
@@ -16,6 +24,14 @@ object NeuralNetwork{
       }
     }
 
+  /**
+    * Calculate an impact of every layer's weight we have in place on a missed target
+    *
+    * @param reversed_weights
+    * @param activations
+    * @param prev_delta
+    * @return deltas
+    */
     def backpropagate(reversed_weights: List[Tensor], activations: List[Tensor], prev_delta: Tensor): List[Tensor] = reversed_weights match {
       case Nil => List(prev_delta)
       case weight :: Nil => List(prev_delta)
@@ -26,8 +42,14 @@ object NeuralNetwork{
       }
     }
 
-
-
+  /**
+    * Entry point to manage a calculation of weights which used to make a prediction on a dataset
+    *
+    * @param weights
+    * @param dataset
+    * @param alpha
+    * @return weights
+    */
     def learnEpoch(weights: List[Tensor], dataset: List[(Tensor, Tensor)], alpha: Double): List[Tensor] = dataset match {
       case Nil => weights
       case (datapoint, target) :: dataset_tail => {
@@ -52,18 +74,37 @@ object NeuralNetwork{
       }
     }
 
+
+    ////////////////////////
+    // Helper function
+    ///////////////////////
+
+  /**
+    * Calculate an index of missing a target
+    *
+    * @param weights
+    * @param predictions
+    * @param label_set
+    * @return error magnitude
+    */
     def loss(weights: List[Tensor], predictions: Tensor)(implicit label_set: Tensor): Double = weights match {
       case Nil => 0.5 * np.sum((predictions - label_set) ** 2)
       case weight :: weights_tail =>
         loss(weights_tail, 1/(1 + np.exp(-np.dot(predictions, weight))))
     }
 
+  /**
+    * do a prediction on dataset
+    *
+    * @param weights
+    * @param prediction
+    * @return prediction index
+    */
     def predict(weights: List[Tensor], prediction: Tensor): Tensor = weights match {
       case Nil => prediction
       case weight :: weights_tail =>
           predict(weights_tail, 1 /( 1 + np.exp(-np.dot(prediction, weight))))
     }
-
 
 }
 
